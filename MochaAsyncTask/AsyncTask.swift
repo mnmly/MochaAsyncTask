9ee8b0b999c3b5e60672f821434971b3029123e9
mnmly <i.am@mnmly.com>
2017-03-24 19:51:36 -0700

    Initial commit

---
+++ MochaAsyncTask/AsyncTask.swift
@@ -0,0 +1,61 @@
+//
+//  AsyncTask.swift
+//  MochaAsyncTask
+//
+//  Created by Hiroaki Yamane on 3/24/17.
+//  Copyright © 2017 Hiroaki Yamane. All rights reserved.
+//
+
+import Foundation
+
+@objc public protocol AsyncTaskProtocol {
+    func onData(_ data: String) -> Void
+    func onEnd() -> Void
+    func onTermination() -> Void
+}
+
+@objc public class AsyncTask: NSObject {
+    
+    @objc public var delegate: AsyncTaskProtocol?
+    
+    public func exec(launchPath: String, arguments: [String]?) {
+        
+        let task = Process()
+        task.launchPath = launchPath
+        task.arguments = arguments
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        let outHandle = pipe.fileHandleForReading
+        outHandle.waitForDataInBackgroundAndNotify()
+        
+        var progressObserver : NSObjectProtocol!
+        progressObserver = NotificationCenter.default.addObserver(
+            forName: NSNotification.Name.NSFileHandleDataAvailable,
+            object: outHandle, queue: nil) { notification -> Void in
+            let data = outHandle.availableData
+            
+            if data.count > 0 {
+                if let str = String(data: data, encoding: String.Encoding.utf8) {
+                    self.delegate?.onData(str)
+                }
+                outHandle.waitForDataInBackgroundAndNotify()
+            } else {
+                NotificationCenter.default.removeObserver(progressObserver)
+                self.delegate?.onEnd()
+            }
+        }
+        
+        var terminationObserver : NSObjectProtocol!
+        terminationObserver = NotificationCenter.default.addObserver(
+            forName: Process.didTerminateNotification,
+            object: task, queue: nil)
+        {
+            notification -> Void in
+            NotificationCenter.default.removeObserver(terminationObserver)
+            self.delegate?.onTermination()
+        }
+        
+        task.launch()
+    }
+}
---
+++ MochaAsyncTask/MochaAsyncTask.h
@@ -0,0 +1,19 @@
+//
+//  MochaAsyncTask.h
+//  MochaAsyncTask
+//
+//  Created by Hiroaki Yamane on 3/24/17.
+//  Copyright © 2017 Hiroaki Yamane. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MochaAsyncTask.
+FOUNDATION_EXPORT double MochaAsyncTaskVersionNumber;
+
+//! Project version string for MochaAsyncTask.
+FOUNDATION_EXPORT const unsigned char MochaAsyncTaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MochaAsyncTask/PublicHeader.h>
+
+
